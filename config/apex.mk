hentaiOS/platform_vendor_hentai

gms: Use DEV certificates flag for networkstack perm
* For some odd reason it dosen't get included on Pixel 2 otherwise

config/apex.mk
@@ -14,6 +14,9 @@
 # limitations under the License.
 #
 
+# Networkstack certificate
+PRODUCT_MAINLINE_SEPOLICY_DEV_CERTIFICATES := vendor/hentai/apex/NetworkStack
+
 # Enable Google Play system updates support
 PRODUCT_SOONG_NAMESPACES += \
     vendor/hentai/apex

BasharatKar/vendor_blaze

Signed-off-by: Joey Huab <joey@evolution-x.org>

config/apex.mk
@@ -21,6 +21,9 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
 DEVICE_PACKAGE_OVERLAYS += \
     vendor/blaze/apex/overlay/common
 
+# Networkstack certificate
+PRODUCT_MAINLINE_SEPOLICY_DEV_CERTIFICATES := vendor/evolution/apex/NetworkStack
+
 # Enable Google Play system updates support
 PRODUCT_SOONG_NAMESPACES += \
     vendor/blaze/apex